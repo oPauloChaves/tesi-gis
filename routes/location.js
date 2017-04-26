e9ea9a24e0c9dbf1706fd8d6ed0cbb7b5f5e13ba
Kelvin campelo Alves de sousa <kelvincampelo@Kelvins-MacBook-Pro.local>
2017-04-26 19:23:07 -0300

    Commented the locations routes file

--- routes/location.js
+++ routes/location.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const Location = require('../models/Location');
 
-/* GET locations listing. */
+/** 
+ * GET locations listing.
+ * nome filter (ex.: ?nome=Bar)
+ */
 router.get('/', (req, res, next) => {
     console.log(JSON.stringify(req.query));
     let query = {}
@@ -11,7 +14,11 @@ router.get('/', (req, res, next) => {
     Location.find(query, (err, locations) => err ? res.status(500).send(err) : locations ? res.status(200).send(locations) : res.sendStatus(404))
 });
 
+/**
+ * GET locaton by id
+ */
 router.get('/:id', (req, res, next) => Location.findOne({ _id: req.params.id }, (err, location) => err ? res.status(500).send(err) : location ? res.status(200).send(location) : res.sendStatus(404)));
+
 /** 
  * POST create new locations
  */
@@ -24,8 +31,12 @@ router.post('/', (req, res, next) => {
     }
 })
 
+/**
+ * GET the Locations near by Location
+ * filter max distance (ex.:?distance=3)
+ */
 router.get('/near/:id', (req, res, next) => Location.findOne({ _id: req.params.id }, (err, location) => {
-    Location.find({ loc: { $near: location.loc } }, (err, nears) => err ? res.status(500).send(err) : nears ? res.status(200).send(nears) : res.sendStatus(404));
+    Location.find({ loc: { $near: location.loc, $maxDistance: req.query.distance || 1 } }, (err, nears) => err ? res.status(500).send(err) : nears ? res.status(200).send(nears) : res.sendStatus(404));
 }));
 
 module.exports = router;
