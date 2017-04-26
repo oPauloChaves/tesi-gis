83ae06e6fabc7604a7c142a8cfca2e02b03301f7
Victor Frazão <vflaragao@gmail.com>
2017-04-26 19:08:42 -0300

    Cleanup and adapt to material design structure

--- public/scripts/openlayers.js
+++ public/scripts/openlayers.js
@@ -1,38 +1,43 @@
+/** Background map layer */
 var raster = new ol.layer.Tile({
     source: new ol.source.OSM()
 });
-var source = new ol.source.Vector({wrapX: false});
+
+/** GeoObjects over map layer */
+var source = new ol.source.Vector();
+
+/** GeoObjects layer */
 var vector = new ol.layer.Vector({
     source: source
 });
+
+/** Map setup */
 var map = new ol.Map({
     layers: [raster, vector],
     target: 'map',
     view: new ol.View({
-    center: [-4765694, -567410],
-    zoom: 15
+        center: [-4765694, -567410],
+        zoom: 15
     })
 });
-var typeSelect = document.getElementById('type');
-var draw; // global so we can remove it later
 
-function addInteraction() {
-    var value = typeSelect.value;
-    if (value !== 'None') {
+/** Draw instance */
+var draw;
+
+/** Method to insert a new object into map */
+function insertObject(type) {
     draw = new ol.interaction.Draw({
         source: source,
-        type: /** @type {ol.geom.GeometryType} */ (typeSelect.value)
+        type: type
     });
+    draw.on('drawend', finishObject)
     map.addInteraction(draw);
-    }
 }
 
-/**
-* Handle change event.
-*/
-typeSelect.onchange = function() {
-    map.removeInteraction(draw);
-    addInteraction();
-};
-
-addInteraction();
+/** Callback dispatched when geoObject is drawed */
+function finishObject(event) {
+    var obj = event.feature.getGeometry();
+    console.log('TYPE', obj.getType());
+    console.log('COORDINATES', obj.getCoordinates());
+    draw = null;
+}
