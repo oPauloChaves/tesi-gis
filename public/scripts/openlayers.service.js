019e37020e8c30b35d085536d3a6f7bcdb82bf3f
Victor Frazão <vflaragao@gmail.com>
2017-04-27 18:38:50 -0300

    Inserts handle functions to openlayer service

--- public/scripts/openlayers.service.js
+++ public/scripts/openlayers.service.js
@@ -32,6 +32,12 @@ function openLayers() {
     });
 
     vm.draw = draw;
+    vm.parseFeature = parseFeature;
+    vm.parseCoordinate = parseCoordinate;
+
+    vm.addFeature = addFeature;
+    vm.editFeature = editFeature;
+    vm.removeFeature = removeFeature;
 
     //////////////////////////////////////
 
@@ -42,5 +48,52 @@ function openLayers() {
         });
         vm.drawStrategy.on('drawend', cb)
         vm.map.addInteraction(vm.drawStrategy);
+    }
+
+    function removeFeature(feature) {
+        var feature = !feature ? vm.source.getFeatures().pop() : feature;
+        vm.source.removeFeature(feature);
+    }
+
+    function addFeature(feature) {
+        vm.source.addFeature(feature);
+    }
+
+    function editFeature(feature) {
+        removeFeature(feature);
+        addFeature(feature);
+    }
+
+    function parseCoordinate(rawCoordinates) {
+        var geojsonFormat = new ol.format.GeoJSON();
+        return geojsonFormat.writeGeometryObject(rawCoordinates);
+    }
+
+    function parseFeature(rawFeature) {
+        try {
+            if(rawFeature instanceof Array) {
+                return rawFeature.map(_parseFeature);
+            }
+        } catch(e) {
+            console.error('IGNORE', e)
+        }
+        return _parseFeature(rawFeature);
+    }
+
+    function _parseFeature(rawFeature) {
+        switch(rawFeature.loc.type) {
+            case 'Point':
+                rawFeature.loc = new ol.geom.Point(rawFeature.loc.coordinates);
+                return rawFeature;
+            case 'Polygon':
+                rawFeature.loc = new ol.geom.Polygon(rawFeature.loc.coordinates);
+                return rawFeature;
+            case 'LineString':
+                rawFeature.loc = new ol.geom.LineString(rawFeature.loc.coordinates);
+                return rawFeature;
+            case 'MultiLineString':
+                rawFeature.loc = new ol.geom.MultiLineString(rawFeature.loc.coordinates);
+                return rawFeature;
+        }
     }
 }
