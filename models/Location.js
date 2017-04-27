76f96ae88988e2ecf5574572357ade38ddf2e505
Kelvin campelo Alves de sousa <kelvincampelo@Kelvins-MacBook-Pro.local>
2017-04-27 19:30:06 -0300

    Added ente and categoria attr into Location model

--- models/Location.js
+++ models/Location.js
@@ -4,12 +4,16 @@ const Schema = moongose.Schema;
 const LocationSchema = new Schema({
     nome: { type: String },
     descricao: { type: String },
+
+    ente: { type: String },
     denominacao: { type: String },
+    categoria: { tyep: String },
+
     loc: {
         type: { type: String },
         coordinates: []
     }
-});
+}, { strict: false });
 
 LocationSchema.index({ loc: '2dsphere' });
 
