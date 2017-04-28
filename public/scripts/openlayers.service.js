b0990eda82f61c8c48e74d005f93469c023afa74
Paulo Chaves <opaulochaves@gmail.com>
2017-04-28 01:09:41 -0300

    add modify, select and hightlight features

--- public/scripts/openlayers.service.js
+++ public/scripts/openlayers.service.js
@@ -24,8 +24,9 @@ function openLayers() {
             source: new ol.source.OSM()
         });
 
+        var features = new ol.Collection(listFeatures);
         /** GeoObjects over map layer */
-        vm.source = new ol.source.Vector({ features: listFeatures });
+        vm.source = new ol.source.Vector({ features: features });
 
         /** GeoObjects layer */
         vm.vector = new ol.layer.Vector({
@@ -44,6 +45,10 @@ function openLayers() {
                 zoom: 15
             })
         });
+
+        highlightFeature();
+        selectFeature();
+        modifyFeature(features);
     }
 
     function draw(geoType, cb) {
@@ -88,5 +93,106 @@ function openLayers() {
     function _parseFeature(rawFeature) {
         var geojsonFormat = new ol.format.GeoJSON();
         return geojsonFormat.readFeature(rawFeature.loc);
+    }
+
+    /* Highlight feature under the cursor */
+    function highlightFeature() {
+      var highlightStyleCache = {};
+
+      var featureOverlay = new ol.layer.Vector({
+        source: new ol.source.Vector(),
+        map: vm.map,
+        style: function (feature, resolution) {
+          var text = resolution < 5000 ? feature.get('name') : '';
+          if (!highlightStyleCache[text]) {
+            highlightStyleCache[text] = new ol.style.Style({
+              stroke: new ol.style.Stroke({
+                color: '#f00',
+                width: 2
+              }),
+              fill: new ol.style.Fill({
+                color: 'rgba(240,215,215,0.1)'
+              })
+            });
+          }
+          return highlightStyleCache[text];
+        }
+      });
+
+      var highlight;
+      var displayFeatureInfo = function (pixel) {
+
+        var feature = vm.map.forEachFeatureAtPixel(pixel, function (feature) {
+          return feature;
+        });
+
+        /*var info = document.getElementById('info');
+        if (feature) {
+          info.innerHTML = feature.getId() + ': ' + feature.get('name');
+        } else {
+          info.innerHTML = '&nbsp;';
+        }*/
+
+        if (feature !== highlight) {
+          if (highlight) {
+            featureOverlay.getSource().removeFeature(highlight);
+          }
+          if (feature) {
+            featureOverlay.getSource().addFeature(feature);
+          }
+          highlight = feature;
+        }
+
+      };
+
+      vm.map.on('pointermove', function (evt) {
+        if (evt.dragging) {
+          return;
+        }
+        var pixel = vm.map.getEventPixel(evt.originalEvent);
+        displayFeatureInfo(pixel);
+      });
+
+      vm.map.on('click', function (evt) {
+        displayFeatureInfo(evt.pixel);
+      });
+    }
+
+    /* Select an object when clicking on it */
+    function selectFeature() {
+      // a normal select interaction to handle click
+      var select = new ol.interaction.Select({
+        style: new ol.style.Style({
+          stroke: new ol.style.Stroke({
+            color: '#f00',
+            width: 2
+          }),
+          fill: new ol.style.Fill({
+            color: 'rgba(240,215,215,0.1)'
+          })
+        })
+      });
+      vm.map.addInteraction(select);
+    }
+
+    /**
+     * Modify existing features.
+     * You may add new vertices or delete them.
+     * To delete a vertice, hold SHIFT and click on a vertice
+     *
+     * @param {any} features
+     */
+    function modifyFeature(features) {
+      var modify = new ol.interaction.Modify({
+        features: features,
+        // the SHIFT key must be pressed to delete vertices, so
+        // that new vertices can be drawn at the same position
+        // of existing vertices
+        deleteCondition: function (event) {
+          return ol.events.condition.shiftKeyOnly(event) &&
+            ol.events.condition.singleClick(event);
+        }
+      });
+      vm.map.addInteraction(modify);
     }
 }
